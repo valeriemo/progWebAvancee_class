bc47a7ebf42de51f5dc354edc8b383d9cc51f2f4
valeriemo <v_moreau@hotmail.com>
2024-01-12 09:54:56 -0500

    installation vue et node

---
+++ project-vue/client/src/main.js
@@ -0,0 +1,6 @@
+// Point d'entrée de l'application
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+createApp(App).use(router).mount('#app')
---
+++ project-vue/client/src/views/TestView.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Test View</h1>
+        <p>This is a test view.</p>
+    </div>
+</template>
